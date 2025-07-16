b83b6be3aaeecc78ddd4dc5a5f6a65d53a9ff0d9
forhad1518 <ahforhad21@gmail.com>
2025-07-16 22:30:50 +0600

    Initial commit

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 dotenv.config();
-const express = require('express');
-const { default: connectDB } = require('./config/mongoos');
-const cors = require('cors');
+import express from 'express';
+import connectDB from './config/mongoos.js';
+import cashRoutes from './routes/Cash.routes.js';
+import cors from 'cors';
+
+// Initialize express app
 const app = express();
 const port = 3000;
 connectDB()
 
-// app.use(cors());
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello fahima!');
+});
+
+// import routes
+app.use('/api', cashRoutes);
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
 });
 
 
